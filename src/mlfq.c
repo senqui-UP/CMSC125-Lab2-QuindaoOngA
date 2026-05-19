a6e63ff333bcf177130e7f3d7292a7334f65f5d6
aXONG476 <andydominicong765@gmail.com>
2026-05-20 01:19:31 +0800

    [Chore] add scheduler implementation stubs

---
+++ src/mlfq.c
@@ -0,0 +1,14 @@
+// Multi Level Feedback Queue Implementation
+
+#include <stdio.h>
+#include "../include/process.h"
+#include "../include/scheduler.h"
+
+void schedule_mlfq(Process processes[], int n, int quantum)
+{
+    (void)processes;
+    (void)n;
+    (void)quantum;
+
+    printf("MLFQ selected\n");
+}
---
+++ src/stcf.c
@@ -0,0 +1,14 @@
+// Shortest Time to Completion First Implementation
+
+#include <stdio.h>
+#include "../include/process.h"
+#include "../include/scheduler.h"
+
+void schedule_stcf(Process processes[], int n, int quantum)
+{
+    (void)processes;
+    (void)n;
+    (void)quantum;
+
+    printf("STCF selected\n");
+}
